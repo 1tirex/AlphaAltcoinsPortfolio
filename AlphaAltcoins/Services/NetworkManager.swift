c6332ddf20bfdbf36a960cc2713a0a875f00bca1
1tirex <108235206+1tirex@users.noreply.github.com>
2022-12-13 18:24:25 +0400

    create Network Manager with Alomofire

---
+++ AlphaAltcoins/Services/NetworkManager.swift
@@ -0,0 +1,33 @@
+//
+//  NetworkManager.swift
+//  AlphaAltcoins
+//
+//  Created by Илья on 13.12.2022.
+//
+
+import Foundation
+import Alamofire
+
+final class NetworkManager {
+    static let shared = NetworkManager()
+    private init() {}
+    
+    func fetch<T: Decodable>(type: T.Type,
+                             needFor: CreateLink.TypeLink,
+                             coin name: String? = "",
+                             completion: @escaping(Result<T, AFError>) -> Void) {
+        
+        let link = CreateLink(needLinkFor: needFor, baseAsset: name ?? "")
+        
+        AF.request(link.url)
+            .validate()
+            .responseDecodable(of: T.self) { dataResponse in
+                switch dataResponse.result {
+                case .success(let type):
+                    completion(.success(type))
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+            }
+    }
+}
