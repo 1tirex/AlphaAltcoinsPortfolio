fafbadeefe0e3b9e82fedd73d496ee171846440b
1tirex <108235206+1tirex@users.noreply.github.com>
2022-12-13 18:23:24 +0400

    create logic for get some links for fetch data "getLinks"

---
+++ AlphaAltcoins/Services/GetLinks.swift
@@ -0,0 +1,48 @@
+//
+//  GetLinks.swift
+//  AlphaAltcoins
+//
+//  Created by Илья on 13.12.2022.
+//
+
+import Foundation
+
+public struct CreateLink {
+    
+    public let needLinkFor: TypeLink
+    public let coin: String
+    public let url: String
+    
+    // MARK: Initialization
+    public init(needLinkFor: TypeLink, baseAsset coin: String = "") {
+        self.needLinkFor = needLinkFor
+        self.coin = coin
+        
+        switch needLinkFor {
+        case .markets:
+            let url = "https://cryptingup.com/api/markets?size=10"
+            self.url = url
+        case .marketsAll:
+            let url = "https://cryptingup.com/api/markets?size=all"
+            self.url = url
+        case .coinSearch:
+            let url = "https://cryptingup.com/api/assets/\(coin.uppercased())/markets"
+            self.url = url
+        case .nameCoin:
+            let url = "https://cryptingup.com/api/assetsoverview"
+            self.url = url
+        case .nothing:
+            let url = "https://cryptingup.com/api/markets?size=10"
+            self.url = url
+        case .postRequest:
+            let url = "https://jsonplaceholder.typicode.com/posts"
+            self.url = url
+        }
+    }
+}
+
+public extension CreateLink {
+    enum TypeLink: String {
+        case markets, marketsAll, coinSearch, nameCoin, postRequest, nothing
+    }
+}
